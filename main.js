6ba9d6db09d9ec8d1737bc90df84f91e12beae24
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-01-12 12:06:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+let showInp = document.getElementById("showIp");
+let showthis = document.getElementById("result");
+let btn = document.getElementById("btn");
+let restart = document.getElementById("restart");
+
+let computer = Math.floor(Math.random() * 3);
+let cpu = ["S", "W", "G"][computer];
+
+btn.addEventListener("click", function () {
+    let user = document.getElementById("user-input").value.toUpperCase()[0];
+
+    if (user == "") {
+        alert("Please write S, W or G");
+    }
+    else if (cpu === user) {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>Nobody ! Try again`;
+        showthis.style.color = "red";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "S" && user === "W") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>Computer win`;
+        showthis.style.color = "red";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "S" && user === "G") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>You win`;
+        showthis.style.color = "green";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "W" && user === "G") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>Computer win`;
+        showthis.style.color = "red";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "W" && user === "S") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>You win`;
+        showthis.style.color = "green";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "G" && user === "W") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>You win`;
+        showthis.style.color = "green";
+        restart.style.display = "block";
+
+    }
+    else if (cpu === "G" && user === "S") {
+        showthis.innerHTML = `<b style="color: darkgrey;">Computer: ${cpu} <br> User: ${user}</b><br><br>Computer win`;
+        showthis.style.color = "red";
+        restart.style.display = "block";
+
+    }
+    else {
+        restart.style.display = "none";
+    }
+})
+
+restart.addEventListener("click", function () {
+    window.location.href = "index.html";
+})
